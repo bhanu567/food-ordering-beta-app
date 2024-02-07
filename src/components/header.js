d230f48d9ab802fc087f82d3fbb5f4da6e63f161
bhanu567 <bhanupjha5465@gmail.com>
2024-02-07 10:24:37 +0530

    Added a summary page

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navigation from "./navigation";
 import mealsImage from "../assets/header_image.jpg"
 import "./header.css"
-
+import Summary from "./summary";
 
 const Header = () => {
   return (
     <div >
       <Navigation></Navigation>
       <img className="mealsImage" src={mealsImage} alt="logo"></img>
+      <Summary></Summary>
     </div>
   );
 };
---
+++ src/components/summary.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./summary.css";
+
+const Summary = () => {
+  return (
+    <div className="border_box">
+      <h1>Delicious Food, Delivered to you</h1>
+
+      <p className="first">
+        choose your favourite meal from our broad selection of avaible meals and
+        enjoy a delecious lunch or dinner at home
+      </p>
+
+      <p className="second">
+        All out meals are cooked with high quality ingrediants, just in time and
+        ofcourse by experinced chefs!
+      </p>
+    </div>
+  );
+};
+export default Summary;
